172251a5c20f74ade5d972e65d96d66ecc4e611b
YodoMorfius <vasya2002matveev@gmail.com>
2019-07-04 17:35:15 +0500

    First commit

--- first_bot.py
+++ first_bot.py
@@ -4,10 +4,15 @@ from config import *
 
 vkBot = vk_api.VkApi(token = BOT_TOKEN)
 
-USER_ID = 461394843
-text = 'Это сообщение отправлено с помощью vk_api'
-result = vkBot.method('messages.send',
-{'user_id' : USER_ID,
- 'random_id' : random.randint(1, 2147483648),
- 'message' : text})
-print(result)
+def write_msg(user_id, text):
+    vkBot.method('messages.send',
+                 {'user_id': user_id,
+                  'message': text,
+                  'random_id': random.randint(0, 2147483648)})
+
+lp_server=vkBot.method('messages.getLongPollServer',
+                       {'lp_version': 3,
+                        'need_pts' : 0})
+print(lp_server)
+ts, server, key=lp_server['ts'], lp_server['key'], lp_server['server']
+write_msg(461394843, 'Привет!')
